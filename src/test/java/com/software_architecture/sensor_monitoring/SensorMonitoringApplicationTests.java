843900e67058e494f790a8250ef7d1a84f757532
LakshanViranga <lakshanviranga0000@gmail.com>
2021-04-21 14:56:08 +0530

    Initiated

---
+++ src/test/java/com/software_architecture/sensor_monitoring/SensorMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software_architecture.sensor_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
